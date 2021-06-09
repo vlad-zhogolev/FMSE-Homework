738a371636c0c68d6fb52adebec919e823b49aa4
zvladyuha <z.vladyuha@yandex.ru>
2021-06-10 01:32:16 +0300

    +

--- verify.bat
+++ verify.bat
@@ -1,19 +1,19 @@
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_0_1 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_0_2 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_0_3 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_1_3 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_2_3 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_2_4 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_2_5 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_3_5 crossroads_round.pml
-@REM spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_4_5 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_0_1 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_0_2 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_0_3 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_1_3 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_2_3 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_2_4 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_2_5 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_3_5 crossroads_round.pml
+spin -search -a -m1000000 -w27 -DCOLLAPSE -ltl safety_4_5 crossroads_round.pml
 
-spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_0 crossroads_round.pml
-spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_1 crossroads_round.pml
-spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_2 crossroads_round.pml
-spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_3 crossroads_round.pml
-spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_4 crossroads_round.pml
-spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_5 crossroads_round.pml
+@REM spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_0 crossroads_round.pml
+@REM spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_1 crossroads_round.pml
+@REM spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_2 crossroads_round.pml
+@REM spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_3 crossroads_round.pml
+@REM spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_4 crossroads_round.pml
+@REM spin -search -a -b -m1000000 -w27 -DMA=260 -DNOREDUCE -ltl liveness_5 crossroads_round.pml
 
 
 @REM spin -search -a -b -m1000000 -w27 -DCOLLAPSE -ltl fairness_0 crossroads_round.pml
